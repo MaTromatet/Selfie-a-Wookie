21601198960d6da9adb76009cc5bd560b2c98cf2
MaTromatet <mromatet@gmail.com>
2022-08-06 11:20:07 +0200

    Ajout de modules partagés

--- 07 - Découvrons Angular/maPremiereApplication/src/app/app.module.ts
+++ 07 - Découvrons Angular/maPremiereApplication/src/app/app.module.ts
@@ -4,21 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MenuGeneralComponent } from './shared/components/menu-general/menu-general.component';
-import { SelfieListComponent } from './feature/selfies/selfie-list/selfie-list.component';
-import { UnSelfieReadonlyComponent } from './feature/selfies/un-selfie-readonly/un-selfie-readonly.component';
 import { LoggerService } from './shared/services/logger/logger.service';
-import { AddSelfiesComponent } from './feature/selfies/add-selfies/add-selfies.component';
-import { FormsModule } from '@angular/forms';
+import { SelfieModule } from './feature/selfies/selfie.module';
+import { SharedModule } from './shared/modules/shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuGeneralComponent,
-    SelfieListComponent,
-    UnSelfieReadonlyComponent,
-    AddSelfiesComponent,
+    MenuGeneralComponent
   ],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, HttpClientModule,SelfieModule,SharedModule],
   providers: [
     //LoggerService
   ],
---
+++ 07 - Découvrons Angular/maPremiereApplication/src/app/feature/selfies/selfie.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SelfieListComponent } from './selfie-list/selfie-list.component';
+import { AddSelfiesComponent } from './add-selfies/add-selfies.component';
+import { UnSelfieReadonlyComponent } from './un-selfie-readonly/un-selfie-readonly.component';
+import { SharedModule } from 'src/app/shared/modules/shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    SelfieListComponent,
+    UnSelfieReadonlyComponent,
+    AddSelfiesComponent
+  ],
+  imports: [
+    CommonModule, SharedModule
+  ],
+  exports: [
+    SelfieListComponent,
+  ],
+})
+export class SelfieModule { }
